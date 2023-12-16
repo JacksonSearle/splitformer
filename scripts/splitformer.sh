b6de17d98debd34f3c6d789ab894c669763e84e6
Jackson Searle <jacksonsearle24@gmail.com>
2023-12-16 15:43:37 -0700

    official tests

--- scripts/splitformer.sh
+++ scripts/splitformer.sh
@@ -1,18 +1,33 @@
+#!/bin/bash
+
+#SBATCH --time=12:00:00   # walltime
+#SBATCH --ntasks=4   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=8192M   # memory per CPU core
+#SBATCH --qos=cs
+
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
 python3 ../train_model.py \
-    --activation-dropout 0.0 \
+    --activation-dropout 0.2 \
     --checkpoint-activations False \
     --dropout 0.0 \
-    --embed-dim 32 \
-    --ffn-dim 64 \
+    --embed-dim 128 \
+    --ffn-dim 1024 \
     --fsdp True \
-    --layers 1 \
+    --layers 8 \
     --lr 0.001 \
     --model splitformer \
     --heads 8 \
-    --seq-len 128 \
-    --value-embed-dim 32 \
+    --seq-len 512 \
+    --value-embed-dim 128 \
     --vocab-size 28783 \
-    --device cpu \
-    --epochs 10 \
-    --batch-size 128 \
-    --tokens-per-pass 2 \
+    --device cuda \
+    --epochs 30 \
+    --batch-size 16 \
+    --tokens-per-pass 10 \
